b70149a2b6de8e2feaf45fad3ee589eaec223528
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2024-03-02 23:03:41 +0300

    Add files via upload

---
+++ CODING PAGE/main.js
@@ -0,0 +1,8 @@
+const ratings = [
+    { emoji: "", name: "Give us rating" },
+    { emoji: "", name: "Very Poor" },
+    { emoji: "", name: "Poor" },
+    { emoji: "", name: "Good" },
+    { emoji: "", name: "Very Good" },
+    { emoji: "", name: "Excellent" },
+]
