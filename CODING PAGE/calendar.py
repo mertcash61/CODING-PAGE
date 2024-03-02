0f02890bf4c3c6bf63fb8ad690711c4aa8df8e3f
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2024-03-02 23:03:57 +0300

    Delete CODING PAGE directory

--- CODING PAGE/calendar.py
+++
@@ -1,5 +0,0 @@
-import calendar
-yy = 2012  # year
-mm = 12    # month
-# display the calendar
-print(calendar.month(yy, mm))
--- CODING PAGE/main.js
+++
@@ -1,8 +0,0 @@
-const ratings = [
-    { emoji: "", name: "Give us rating" },
-    { emoji: "", name: "Very Poor" },
-    { emoji: "", name: "Poor" },
-    { emoji: "", name: "Good" },
-    { emoji: "", name: "Very Good" },
-    { emoji: "", name: "Excellent" },
-]
